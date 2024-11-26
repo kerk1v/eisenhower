f73fc0de3d2c94eeed9dcf492c3b299844602b2c
Volker Kerkhoff (SRE) <volker.kerkhoff@payconiq.com>
2024-11-26 17:47:26 +0100

    Make links in description clickable

--- matrix/templatetags/matrix_extras.py
+++ matrix/templatetags/matrix_extras.py
@@ -1,13 +1,19 @@
 from django import template
-from datetime import datetime, timedelta
-from django.utils import timezone
+from django.utils.html import format_html
+import re
 
 register = template.Library()
 
-@register.filter
-def is_due_soon(value):
-    if not value:
-        return False
-    now = timezone.now()
-    two_days_from_now = now + timedelta(days=2)
-    return value <= two_days_from_now
+@register.filter(name='urlize_custom')
+def urlize_custom(text):
+    """Convert URLs in text into clickable links."""
+    url_pattern = r'(https?://[^\s<>"]+|www\.[^\s<>"]+)'
+    
+    def replace_url(match):
+        url = match.group(0)
+        display_url = url
+        if not url.startswith(('http://', 'https://')):
+            url = 'http://' + url
+        return format_html('<a href="{}" target="_blank" rel="noopener noreferrer" class="text-blue-600 hover:text-blue-800 underline">{}</a>', url, display_url)
+    
+    return format_html(re.sub(url_pattern, replace_url, text or ''))
